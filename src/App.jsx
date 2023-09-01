d5166caae8a9c662a17c933da9ef4e24599a1fd4
Irfan Ahmad <irfanft12@gmail.com>
2023-09-01 15:36:30 +0530

    logo and favicon changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import AddTodo from './components/AddTodo'
 import TodoList from './components/TodoList'
+import logo from "/logo.svg"
 
 function App() {
 
   return (
-    <main>
-      <AddTodo />
-      <TodoList />
-    </main>
+    <>
+      <header>
+        <img src={logo} alt="" />
+        <h1 className="logo-text">Todo List</h1>
+      </header>
+      <main>
+        <AddTodo />
+        <TodoList />
+      </main>
+    </>
+    
   )
 }
 
